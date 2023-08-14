413078923aef5f68d8a4db3a80180f208a60410a
Federico Maciel <federicomaciel.dev@gmail.com>
2023-08-13 22:22:46 -0300

    Adding basic config for hibernate, hope it works

--- src/main/java/org/FmFvGMA/App.java
+++ src/main/java/org/FmFvGMA/App.java
@@ -4,12 +4,9 @@ import ui.MainWindow;
 
 import javax.swing.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+
     public static void main( String[] args ) {
         // * This ensures that all our swing code runs in the same thread (called the EventDispatchThread)
         // * We must do it this way because Swing isn't thread save (we do this once and never again)
--- src/main/java/ui/MainWindow.java
+++ src/main/java/ui/MainWindow.java
@@ -10,10 +10,10 @@ public class MainWindow extends JFrame{
         // - its size
         // - the location where the window appears (center of the screen)
         // finally we display the window
-        setTitle("No pierdan la volunta wachos");
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setSize(800,500);
-        setLocationRelativeTo(null);
-        setVisible(true);
+        this.setTitle("No pierdan la volunta wachos");
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setSize(800,500);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
     }
 }
