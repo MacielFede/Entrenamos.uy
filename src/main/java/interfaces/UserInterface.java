b34e6beaf1f95ef57b5784459ff3dd424987bd46
fvigliarolo <fd.vigliarolo@gmail.com>
2023-08-28 18:40:15 -0300

    Operaciones ExistEmail y newUser. Para el caso de uso addNewUser

--- src/main/java/interfaces/UserInterface.java
+++ src/main/java/interfaces/UserInterface.java
@@ -1,6 +1,7 @@
 package interfaces;
 
 import dataTypes.DtUser;
+import exceptions.AtributeAlreadyExists;
 import exceptions.EmptyRequiredFieldException;
 import exceptions.FebruaryDayException;
 import exceptions.SameYearException;
@@ -13,4 +14,8 @@ public interface UserInterface {
 	String [] listUsersByNickname();
 	void updateUserInfo(DtUser updatedUser) throws EmptyRequiredFieldException, FebruaryDayException, SameYearException;
 	void newMember(DtUser DtNewUser);
-}
+	public boolean existsNickname(String nickName);
+	public boolean existsEmail(String email);
+	public void newUser(DtUser newUser) throws EmptyRequiredFieldException, FebruaryDayException, SameYearException, AtributeAlreadyExists;
+
+	}
